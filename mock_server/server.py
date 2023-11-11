ad5a067241195ae53a0c9f59d2bf1715bbc20bb2
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-11-11 13:54:25 +0545

    Upload image via chunks and add mock server (WIP). Uploading doesn't work.

---
+++ mock_server/server.py
@@ -0,0 +1,228 @@
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
+import numpy as np
+import cv2
+import fileinput
+import sys
+from pathlib import Path
+import re
+import uuid
+
+module_path = Path(__file__).resolve()
+include_folder = module_path.parents[1].joinpath(
+    "include"
+)  # the include folder of workspace
+print(f"Module path: {module_path}")
+print(f"Include folder: {include_folder.resolve()}")
+
+
+def sanitize_filename(filename: str) -> str:
+    """
+    Replaces all forbidden chars with '' and removes unnecessary whitespaces
+    If, after sanitization, the given filename is empty, the function will return 'file_[UUID][ext]'
+
+    :param filename: filename to be sanitized
+    :return: sanitized filename
+    """
+    chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+
+    filename = filename.translate({ord(x): '' for x in chars}).strip()
+    name = re.sub(r'\.[^.]+$', '', filename)
+    extension = re.search(r'(\.[^.]+$)', filename)
+    extension = extension.group(1) if extension else ''
+
+    return filename if name else f'file_{uuid.uuid4().hex}{extension}'
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.log_request()
+        # send 200 response
+        self.send_response(200)
+        # add our own custom header
+        self.send_header("mock_header_key", "mock_header_value")
+        # send response headers
+        self.end_headers()
+        # send the body of the response
+        self.wfile.write(bytes("Hello to Esp32 from server.", "utf-8"))
+
+    def do_POST(self):
+        self.log_request()
+        rfid_serial_number = int(self.headers.get("rfid-serial-number", 0)) # set rfid to 0 if fail
+        content_type = self.headers.get("Content-Type").__str__()
+        print(content_type)
+        if rfid_serial_number==0:
+            response = 417
+            self.log_message(f"Response {response}")
+            self.send_response(response)  # expectation failed
+            self.wfile.write(f"Expected key `rfid-serial-number` in the header".encode())
+
+        # reading only if image/jpeg was received
+        elif content_type.find("multipart/form-data") > -1:
+            response = 200
+            self.log_message(f"Response {response}")
+            self.send_response(response)
+            self.end_headers()
+            # extract boundary from headers
+            boundary = re.search(f'boundary=([^;]+)', self.headers['Content-Type']).group(1)
+
+            if "Content-Length" in self.headers:
+                # read all bytes (headers included)
+                # 'readlines()' hangs the script because it needs the EOF character to stop,
+                # even if you specify how many bytes to read
+                # 'file.read(nbytes).splitlines(True)' does the trick because 'read()' reads 'nbytes' bytes
+                # and 'splitlines(True)' splits the file into lines and retains the newline character
+                data = self.rfile.read(int(self.headers['Content-Length'])).splitlines(True)
+            elif "chunked" in self.headers.get("Transfer-Encoding", ""):
+                # the chunk buffer
+                data = "".encode()
+                while True:
+
+                    line = self.rfile.readline().strip() # in the form <length-hex>\r\n
+
+                    # skip if only line endings are provided
+                    if len(line) == 0:
+                        continue
+
+                    chunk_length = int(line, 16) # in the hexadecimal format
+
+                    if chunk_length != 0:
+                        chunk = self.rfile.read(chunk_length)
+                        data += chunk
+
+                    # Each chunk is followed by an additional empty newline
+                    # that we have to consume.
+                    self.rfile.readline()
+
+                    # Finally, a chunk size of 0 is an end indication
+                    if chunk_length == 0:
+                        break
+                data.splitlines(True)
+
+            # find all filenames
+            filenames = re.findall(f'{boundary}.+?filename="(.+?)"', str(data))
+
+            if not filenames:
+                return False, 'couldn\'t find file name(s).'
+
+            filenames = [sanitize_filename(filename) for filename in filenames]
+
+            # find all boundary occurrences in data
+            boundary_indices = list((i for i, line in enumerate(data) if re.search(boundary, str(line))))
+
+            # display images
+            for i in range(len(filenames)):
+                # remove file headers
+                file_data = data[(boundary_indices[i] + 4):boundary_indices[i+1]]
+
+                # join list of bytes into bytestring
+                file_data = b''.join(file_data)
+
+                self.log_message(f"Trying to decode the image.")
+                np_arr = np.frombuffer(file_data, np.uint8)
+                cv_img = cv2.imdecode(np_arr, 0)
+                    
+                # respond with received file size and serial number 
+                self.wfile.write(
+                    f"Got image for rfid tag {rfid_serial_number}".encode()
+                )
+
+                display_image_and_wait(cv_img, f"image{rfid_serial_number}")
+            
+        else:
+            response = 415
+            self.log_message(f"Response {response}")
+            self.send_response(response)  # unsupported media type
+            self.wfile.write(
+                f"Unsupported meadia type {content_type}, expected image/jpeg".encode()
+            )
+
+
+def get_ip():
+    """
+    Get the local ip
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("10.254.254.254", 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
+
+
+def set_server_address(file, address):
+    """
+    Search for `#define SERVER_ADDRESS ` in the given file and replace the line with the provided address
+    """
+    string_to_find = "#define SERVER_ADDRESS "
+    found = False
+    msg = ""
+    for line in fileinput.input(file, inplace=1):
+        if line.find(string_to_find) > -1:
+            found = True
+            msg = f"found the string {string_to_find}\n Replacing {line} with "
+            new_line = f'#define SERVER_ADDRESS "{address}" //testing locally \n'
+            msg = msg + new_line + f"in file {file}"
+            sys.stdout.write(new_line)
+
+            if line == new_line:
+                msg = ""
+        else:
+            sys.stdout.write(line)
+
+    if not found:
+        msg = f"Couldn't find the string {string_to_find}"
+
+    print(msg)
+
+
+def display_image_and_wait(img: np.ndarray, img_name: str):
+    """
+    Displays image waits for `n` or `q` key presses. \n
+    n => close the current window \n
+    q => close all the windows \n
+
+    Parameters
+    ---------
+    img_name: The window name
+    """
+    while True:
+        cv2.imshow(img_name, img)
+
+        k = cv2.waitKey(1)
+
+        if k == ord("n"):  # destroy only the current window on pressing key 'n'
+            cv2.destroyWindow(img_name)
+            break
+
+        if k == ord("q"):  # destroy all the open window on pressing key 'q'
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == "__main__":
+    """
+    Opening on 0.0.0.0 and make it accessible across the local network.
+    Log the local ip.
+    """
+
+    address = get_ip()
+    port = 8000
+    header_filename = "globals.h"
+
+    # replacing the address in globals.h
+    set_server_address(
+        include_folder.joinpath(header_filename).resolve(), f"{address}:{port}"
+    )
+    print(f"Opening http server on {address}:{port}")
+    httpd = HTTPServer(("0.0.0.0", port), MyHandler)
+    httpd.serve_forever()
+
+    # cleanup
+    cv2.destroyAllWindows()
--- src/upload.c
+++ src/upload.c
@@ -2,6 +2,7 @@
 #include <sys/param.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 #include "esp_http_client.h"
 #include "esp_timer.h"
@@ -15,12 +16,17 @@
 // local includes
 
 #include "globals.h"
-
+#include "events.h"
+#include "upload.h"
 // --------------
 
-#define SERVER_ADDRESS "192.168.1.107:5555" // testing locally
-#define MAX_HTTP_RECV_BUFFER 512
-#define MAX_HTTP_OUTPUT_BUFFER 2048
+#define HTTP_POST_REQUEST_BODY_SIZE 512   // allocate this on heap
+#define HTTP_POST_REQUEST_HEADER_SIZE 512 // allocate header on heap
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/form-data;boundary=" PART_BOUNDARY;
+static const char *_CONTENT_DISPOSITION = "form-data; name=\"upfile\"; filename=\"%s\"; ";
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_CONTENT_TYPE_HEADER = "Content-Type: multipart/form-data;boundary=" PART_BOUNDARY "\r\n";
 
 static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
@@ -106,8 +112,8 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
         esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
         if (err != 0)
         {
-            ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
-            ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            ESP_LOGI(TAG, "Last esp error code: 0x%X", err);
+            ESP_LOGI(TAG, "Last mbedtls failure: 0x%X", mbedtls_err);
         }
         if (output_buffer != NULL)
         {
@@ -126,78 +132,204 @@ static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
-esp_err_t upload_jpeg(camera_fb_t *fb, rc522_tag_t *rfid_tag)
+inline void send_crlf(esp_http_client_handle_t client)
 {
-    esp_err_t err;
+    esp_http_client_write(client, "\r\n", 2);
+};
 
-    size_t fb_len = 0;
-    int64_t fr_start = esp_timer_get_time();
-    char rfid_sn_as_string[6]; // tag being 5 bytes long
-    sprintf(rfid_sn_as_string, "%" PRIu64, rfid_tag->serial_number);
+void upload_jpeg_task(void *args)
+{
+    u8_t retry = 0;
+    rfid_a_s_event_data_t *event_data = (rfid_a_s_event_data_t *)args;
+
+    // generic filename for now
+    const char *filename = "capture.jpg";
+
+    // if the control reaches this part, the tag and frame buffer should never be null
+    // todo: remove at production
+    assert(event_data->tag != NULL);
+    assert(event_data->fb != NULL);
+
+    // copying the required passed in data as we are unsure of their lifetimes
+    camera_fb_t *fb = malloc(sizeof(camera_fb_t));
+    memcpy(fb, event_data->fb, sizeof(camera_fb_t));
+
+    uint64_t serial_number = event_data->tag->serial_number;
+    esp_err_t err;
+    size_t fb_len; // to store the exact jpeg length
+    int64_t fr_start;
+
+    size_t hlen;
+    char chunk_len_hex[10];
 
     // Declare local_response_buffer with size (MAX_HTTP_OUTPUT_BUFFER + 1) to prevent out of bound access when
     // it is used by functions like strlen(). The buffer should only be used upto size MAX_HTTP_OUTPUT_BUFFER
-    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
-    /**
-     * NOTE: All the configuration parameters for http_client must be spefied either in URL or as host and path parameters.
-     * If host and path parameters are not set, query parameter will be ignored. In such cases,
-     * query parameter should be specified in URL.
-     *
-     * If URL as well as host and path parameters are specified, values of host and path will be considered.
-     */
-    esp_http_client_config_t config = {
-        .url = SERVER_ADDRESS,
-        .method = HTTP_METHOD_POST,
-        .event_handler = NULL,
-        .user_data = local_response_buffer, // Pass address of local buffer to get response
-        .disable_auto_redirect = true,
-    };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1];
 
-    // if the frame buffer is a nullptr, we cannot proceed for upload
-    if (!fb)
+    char *header = calloc(HTTP_POST_REQUEST_HEADER_SIZE + 1, sizeof(char));
+    char *body = calloc(HTTP_POST_REQUEST_BODY_SIZE + 1, sizeof(char));
+    char *temp_buffer = calloc(128 + 1, sizeof(char));
+
+    while (1)
     {
-        esp_http_client_cleanup(client);
-        return ESP_FAIL;
-    }
+        err = ESP_OK;
 
-    // performing post http request
-    esp_http_client_set_url(client, "http://" SERVER_ADDRESS "/post");
-    esp_http_client_set_method(client, HTTP_METHOD_POST);
-    esp_http_client_set_header(client, "Content-Disposition", "inline; filename=capture.jpg");
-    esp_http_client_set_header(client, "Content-Type", "image/jpeg");
-    err = esp_http_client_set_header(client, "rfid-serial-number", rfid_sn_as_string);
+        // clearing the buffer
+        memset(local_response_buffer, 0, MAX_HTTP_OUTPUT_BUFFER + 1);
+        memset(header, 0, HTTP_POST_REQUEST_HEADER_SIZE);
+        memset(body, 0, HTTP_POST_REQUEST_BODY_SIZE);
+        memset(temp_buffer, 0, 128);
 
-    if (err == ESP_OK)
-    {
-        if (fb->format == PIXFORMAT_JPEG)
+        // upload to server
+        ESP_LOGI(TAG, "Uploading jpeg to server: " SERVER_ADDRESS);
+
+        fb_len = 0;
+        fr_start = esp_timer_get_time();
+        ESP_LOGI(TAG, "The rfid tag: %" PRIu64, serial_number);
+
+        /**
+         * NOTE: All the configuration parameters for http_client must be spefied either in URL or as host and path parameters.
+         * If host and path parameters are not set, query parameter will be ignored. In such cases,
+         * query parameter should be specified in URL.
+         *
+         * If URL as well as host and path parameters are specified, values of host and path will be considered.
+         */
+        esp_http_client_config_t config = {
+            .url = "http://" SERVER_ADDRESS "/post",
+            .method = HTTP_METHOD_POST,
+            .event_handler = _http_event_handler,
+            .user_data = local_response_buffer, // Pass address of local buffer to get response
+            .disable_auto_redirect = true,
+        };
+        esp_http_client_handle_t client = esp_http_client_init(&config);
+
+        // if the frame buffer is a nullptr, we cannot proceed for upload
+        // same for client == null
+        if (!fb || !client)
         {
-            fb_len = fb->len;
-            err = esp_http_client_set_post_field(client, (const char *)fb->buf, fb->len);
+            esp_http_client_cleanup(client);
+            break;
+        }
+
+        ESP_LOGI(TAG, "Performing a http POST request");
+
+        // some header
+        sprintf(temp_buffer, "%" PRIu64 "", serial_number);
+        esp_http_client_set_header(client, "rfid-serial-number", temp_buffer);
+        esp_http_client_set_header(client, "Content-Type", _STREAM_CONTENT_TYPE);
+
+        // setup to send data as chunk
+        esp_http_client_open(client, -1); // write_len=-1 sets header "Transfer-Encoding: chunked" and method to POST
+
+        // header stuffs
+        sprintf(temp_buffer, "POST %s HTTP/1.1\r\n", "/post");
+        strcpy(header, temp_buffer);
+        sprintf(temp_buffer, "Host: %s\r\n", SERVER_ADDRESS);
+        strcat(header, temp_buffer);
+        sprintf(temp_buffer, "User-Agent: esp-idf/%d.%d.%d esp32\r\n", ESP_IDF_VERSION_MAJOR, ESP_IDF_VERSION_MINOR, ESP_IDF_VERSION_PATCH);
+        strcat(header, temp_buffer);
+        sprintf(temp_buffer, "Accept: */*\r\n");
+        strcat(header, temp_buffer);
+        sprintf(temp_buffer, _STREAM_CONTENT_TYPE);
+        strcat(header, _CONTENT_TYPE_HEADER);
+        sprintf(temp_buffer, "rfid-serial-number: %" PRIu64 "\r\n", serial_number);
+        strcat(header, temp_buffer);
+
+        ESP_LOGI(TAG, "the sent header is %s", header);
+
+        // the length of header in hex
+        hlen = snprintf(chunk_len_hex, sizeof(chunk_len_hex), "%X", strlen(header));
+        esp_http_client_write(client, strcat(chunk_len_hex, "\r\n"), hlen + 2); // assuming hlen < 8
+
+        // send header
+        esp_http_client_write(client, header, HTTP_POST_REQUEST_HEADER_SIZE);
+        send_crlf(client);
+
+        // start body
+        strcpy(body, _STREAM_BOUNDARY);                       // boundary start
+        sprintf(temp_buffer, _CONTENT_DISPOSITION, filename); // contentent disposition with filename
+        strcat(body, temp_buffer);                            // concatenating to the body
+        sprintf(temp_buffer, "Content-Type: application/octet-stream\r\n\r\n");
+        strcat(body, temp_buffer);
+
+        ESP_LOGI(TAG, "The sent body is %s", body);
+
+        // the length of body in hex
+        hlen = snprintf(chunk_len_hex, sizeof(chunk_len_hex), "%X", strlen(body));
+        esp_http_client_write(client, strcat(chunk_len_hex, "\r\n"), hlen + 2); // assuming hlen < 8
+
+        // send body
+        esp_http_client_write(client, body, HTTP_POST_REQUEST_BODY_SIZE);
+        send_crlf(client);
+
+        if (err == ESP_OK)
+        {
+            if (fb->format == PIXFORMAT_JPEG)
+            {
+                fb_len = fb->len;
+                // the length of frame buffer in hex
+                hlen = snprintf(chunk_len_hex, sizeof(chunk_len_hex), "%X", fb_len);
+                esp_http_client_write(client, strcat(chunk_len_hex, "\r\n"), hlen + 2); // assuming hlen < 8
+                if (-1 == esp_http_client_write(client, (char *)fb, fb_len))
+                { // the frame buffer
+                    ESP_LOGE(TAG, "Couldn't write frame buffer.");
+                }
+            }
+            else
+            {
+                uint8_t *jpeg_buffer;
+                if (ESP_OK == err && frame2jpg(fb, 80, &jpeg_buffer, &fb_len))
+                {
+                    // the length of frame buffer in hex
+                    hlen = snprintf(chunk_len_hex, sizeof(chunk_len_hex), "%X", fb_len);
+                    esp_http_client_write(client, strcat(chunk_len_hex, "\r\n"), hlen + 2); // assuming hlen < 8
+                    if (-1 == esp_http_client_write(client, (char *)jpeg_buffer, fb_len))
+                    { // the frame buffer
+                        ESP_LOGE(TAG, "Couldn't write frame buffer.");
+                    }
+                }
+            }
+            send_crlf(client);
+        }
+
+        // the length of stream boundary in hex
+        hlen = snprintf(chunk_len_hex, sizeof(chunk_len_hex), "%X", strlen(_STREAM_BOUNDARY));
+        esp_http_client_write(client, strcat(chunk_len_hex, "\r\n"), hlen + 2); // assuming hlen < 8
+        esp_http_client_write(client, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        send_crlf(client);
+
+        // end
+        err = esp_http_client_write(client, "0\r\n", 3);
+        send_crlf(client);
+        // // converting fb_len to string
+        // char fb_len_str[(size_t)((ceil(log10(fb_len)) + 1) * sizeof(char))];
+        // sprintf(fb_len_str, "%zu", fb_len);
+
+        // esp_http_client_set_header(client, "Content-Length", fb_len_str);
+        // err = esp_http_client_perform(client);
+        if (err == ESP_OK)
+        {
+            ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %" PRId64,
+                     esp_http_client_get_status_code(client),
+                     esp_http_client_get_content_length(client));
+            int64_t fr_end = esp_timer_get_time();
+            ESP_LOGI(TAG, "JPG: %luKB %lums", (uint32_t)(fb_len / 1024), (uint32_t)((fr_end - fr_start) / 1000));
+
+            break;
         }
         else
         {
-            uint8_t *jpeg_buffer;
-            if (ESP_OK == err && frame2jpg(fb, 80, &jpeg_buffer, &fb_len))
-            {
-                esp_http_client_set_post_field(client, (const char *)jpeg_buffer, fb->len);
-            }
+            ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+            retry += 1;
+
+            if (retry > UPLOAD_RETRY_COUNT)
+                break;
         }
+        esp_http_client_close(client);
+        esp_http_client_cleanup(client);
     }
-
-    err = esp_http_client_perform(client);
-    if (err == ESP_OK)
-    {
-        ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %" PRId64,
-                 esp_http_client_get_status_code(client),
-                 esp_http_client_get_content_length(client));
-        int64_t fr_end = esp_timer_get_time();
-        ESP_LOGI(TAG, "JPG: %luKB %lums", (uint32_t)(fb_len / 1024), (uint32_t)((fr_end - fr_start) / 1000));
-    }
-    else
-    {
-        ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
-    }
-
-    return err;
+    free(fb); // clearing the frame buffer that we created
+    free(body);
+    free(temp_buffer);
+    vTaskDelete(NULL);
 }
